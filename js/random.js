e1c1348faf407e29ef46b48402af9d19ae0136b0
Zakir Hossain <eng.zakirhossain4969@gmail.com>
2022-10-16 23:30:00 +0600

    Random Fuction

---
+++ js/random.js
@@ -0,0 +1,10 @@
+console.log(Math.random().toFixed(2)*10+1)
+
+let upperValue = 6;
+let lowerValue = 0;
+
+// let resultValue = Math.random() * (upperValue - lowerValue);
+// let resultValue = Math.ceil(Math.random() * (upperValue - lowerValue)+1)
+let resultValue = Math.floor(Math.random() * (upperValue - lowerValue)+1)
+
+console.log(resultValue)
