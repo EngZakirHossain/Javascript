c71503f533e472d8583ed4cf0457987ea889e140
Zakir Trimatric <zakirtrimatric@gmail.com>
2022-10-16 15:52:18 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Header line text change
+// let name = prompt('What Your name ?');
+// let age = prompt('Whats Your age ?');
+//
+//
+// document.getElementById('change').innerHTML= 'Hi... I am' +' '+name+' I am '+age+' years old';
+//Data type
+
+let phone = 'onePlus'+7;
+
+document.getElementById('change').innerHTML= phone;
+console.log('onePlus')
+
