c635e6ddeea56ffd711c78e17c3fb623c76e79e0
Zakir Trimatric <zakirtrimatric@gmail.com>
2022-10-16 17:00:35 +0600

    Basic Function and operator

---
+++ js/function.js
@@ -0,0 +1,9 @@
+function userName(name) {
+    console.log('HI.. I am '+name);
+}
+userName('zakir hossain');
+
+function sum(num1, num2) {
+   return  num1 + num2;
+}
+console.log(sum(12,8))
--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 //Header line text change
-// let name = prompt('What Your name ?');
-// let age = prompt('Whats Your age ?');
-//
-//
-// document.getElementById('change').innerHTML= 'Hi... I am' +' '+name+' I am '+age+' years old';
+let name = prompt('What Your name ?');
+let age = prompt('Whats Your age ?');
+
+
+document.getElementById('change').innerHTML= 'Hi... I am' +' '+name+' I am '+age+' years old';
 //Data Array type
 
-// let Phone = ['Apple','Samsung','OnePlus'];
-// Phone[3]='Nokia';
-//
-// document.getElementById('change').innerHTML= Phone[0];
-// console.log(Phone)
+let Model = ['Apple','Samsung','OnePlus'];
+Model[3]='Nokia';
+
+document.getElementById('change').innerHTML= Model[0];
+console.log(Model)
 
 //Data Object type
 
-// let Phone = {
-//     model:'iPhone 11 pro Max',
-//     storage:'256 GB',
-//     color:'Black',
-// };
-//
-// document.getElementById('change').innerHTML= Phone.model;
-// console.log(Phone)
+let Phone = {
+    model:'iPhone 11 pro Max',
+    storage:'256 GB',
+    color:'Black',
+};
+
+document.getElementById('change').innerHTML= Phone.model;
+console.log(Phone)
 
 //Operator Precedence () {} [] / * - + = >
 
