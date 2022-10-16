da3905ba7fdc9ec995e658c9871e5db367a54155
Zakir Hossain <eng.zakirhossain4969@gmail.com>
2022-10-16 20:25:30 +0600

    Math function

---
+++ js/math.js
@@ -0,0 +1,10 @@
+console.log(Math.PI)
+console.log(Math.pow(7,2))
+console.log(Math.round(7.2))
+console.log(Math.sqrt(144));//root
+console.log(Math.abs(-144));// negative value
+console.log(Math.ceil(4.000005)); //top value
+console.log(Math.floor(4.000005)); //lowest value
+console.log(Math.min(0,5,2,5,5,6,8,9)); //min value
+console.log(Math.max(0,5,2,5,5,6,8,9)); //max value
+console.log(Math.random()); //random  value
